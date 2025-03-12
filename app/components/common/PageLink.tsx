ae2407a9a0433121935a5ab992a1453340bded75
DevenderDhami <devender.dhami.99@gmail.com>
2025-03-12 16:39:38 +0530

    slider in pagelink

--- app/components/common/PageLink.tsx
+++ app/components/common/PageLink.tsx
@@ -1,5 +1,10 @@
 import Link from "next/link";
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper/modules";
+import { bannerImages } from "@/app/lib/constants";
 
 interface PageLinkItem {
   name: string;
@@ -9,25 +14,39 @@ interface PageLinkItem {
 interface PageLinksProps {
   items: PageLinkItem[];
   title: string;
-  imageUrl?: string;
+  imageUrl:string
 }
 
-const PageLinks: React.FC<PageLinksProps> = ({ items, title, imageUrl }) => {
+const PageLinks: React.FC<PageLinksProps> = ({ items, title,imageUrl }) => {
   return (
-    <section
-      className="relative h-[200px] flex items-center justify-center bg-cover bg-center"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    >
-      {/* Overlay for better readability */}
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+    <section className="relative h-[400px] flex items-center justify-center w-full">
+      {/* Swiper Carousel */}
+      <div className="absolute inset-0 z-0 w-full h-full">
+        <Swiper
+          modules={[ Autoplay]}
+          slidesPerView={1}
+          autoplay={{ delay: 3000 }}
+          loop={true}
+          className="w-full h-full"
+        >
+          {bannerImages.map((image, index) => (
+            <SwiperSlide key={index}>
+              <div className="w-full h-full flex justify-center items-center">
+                <img
+                  src={image?.imgSrc}
+                  alt={`Slide ${index + 1}`}
+                  className="w-full h-full object-fit" 
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
 
-      {/* Content */}
-      <div className="relative  text-white text-center">
+      <div className="absolute inset-0 opacity-50 "></div>
+
+      <div className="relative text-white text-center z-20">
         <h3 className="text-4xl font-bold capitalize">{title}</h3>
-
-        {/* Breadcrumbs */}
         <div className="flex justify-center items-center gap-2 mt-2 text-lg font-semibold">
           {items.map((item, index) => (
             <div key={index} className="capitalize">
